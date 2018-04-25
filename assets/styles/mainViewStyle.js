4ffb3347a37d5546fce2345d1c1597e3f40c1342
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-04-25 18:54:12 -0500

    image background style

---
+++ assets/styles/mainViewStyle.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet
+} from 'react-native';
+
+//A continuación se definen los estilos de la pantalla de Login
+
+const mainViewStyle = StyleSheet.create({
+  backGroundImage: {
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    width: undefined,
+    height: undefined
+  },
+})
+
+export default mainViewStyle
