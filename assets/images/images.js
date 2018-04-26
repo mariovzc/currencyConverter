afe24a5cafce395a0262f26ae44f374f225b3c27
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-04-25 20:30:44 -0500

    added the converter view images

--- assets/images/images.js
+++ assets/images/images.js
@@ -1,6 +1,9 @@
 const images = {
 	mainBg: require('./main_bg.png'),
-	icon: require('./icon.png')
+	icon: require('./icon.png'),
+	converterBg: require('./converterBg.png'),
+	deleteIcon: require('./deleteIcon.png'),
+	changeButton: require('./change_button.png')
 }
 
 export default images
